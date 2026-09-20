4b5e760c80c15ebfdcd4a844b81be0199f5e024d
sbvandervorst <sbvandervorst@mail.mccneb.edu>
2026-09-20 19:07:36 +0000

    changed name back

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -473,7 +473,7 @@ export default function Home() {
                     lineHeight: 1.2,
                   }}
                 >
-                  Hi, I&apos;m Seth Vander Vorst
+                  Hi, I&apos;m John Doe
                   {/* Job title with gradient effect */}
                   <Typography
                     variant="h2"
